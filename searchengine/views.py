d16842c84c820a49f0a94190df2dba5059247de4
Ale-SaP <85176998+Ale-SaP@users.noreply.github.com>
2022-04-27 16:58:33 -0300

    first commit

--- searchengine/views.py
+++ searchengine/views.py
@@ -16,51 +16,66 @@ class website():
         self.chosenClass = chosenClass
         self.scrapingStyle = scrapingType
     
+    #Returns = same argument or self.link + argument
+    #If the argument does not start with http or https, it adds self.link to it.
+    #Meant to check if the element obtained in scraping is actually a link or an ID.
     def checkIfHasLink(self, argument):
         if ( (argument.startswith("http:/") == True) or (argument.startswith("https:/") == True) ):
             return(argument)
         else: 
             return(self.link + argument)
 
+    #Returns a list of lists, these lists contain 2 elements, a headline and a link.
+    #List -> Lists -> Head and Link 
     def scraping(self, argument):
-        # "~°Ñ°~" is placed where the search term should be, so we can replace it with the argument here
+        # This variable is defined to be able to reset the value of searchLink at the end of the function
+        originalSearchLink = self.searchLink
+
+         # "~°Ñ°~" is placed where the search term should be, so we can replace it with the argument here
         self.searchLink = self.searchLink.replace("~°Ñ°~", argument)
 
         r = requests.get(self.searchLink)
         soup = BeautifulSoup(r.content, "lxml")
 
-        content = []
+        contentO = []
         
         if (self.scrapingStyle == "basic") :
-            #Getting the headlines and links, the most compatible way I found, since it can find the link in an element's children
-            #Why so? because using the iteration 2 times, once for heads and once for links was slower and more network intensive
-            #Also, neither the .get("href") and ["href"] methods were working, just kinda made my own
+            #Neither the .get("href") and ["href"] methods were working, just kinda made my own
             allHtml = soup.find_all(f"{self.tag}", class_= f"{self.chosenClass}")
 
             for x in allHtml:
 
                 head = (x.get_text()).strip()
-
                 x = (str(x)).split('"')
+
+                #After getting the headline, if for some reason getting a link is impossible, it breaks.
                 try:
                     indexNumber = x.index(" href=") + 1
+                    #Best way of getting the index of the link, due to href being one element away from it.
                 except: break
 
                 link = (self.checkIfHasLink(x[indexNumber]))
-                content.append([head, link])
+
+                #Appending a list with the headline and link.
+                contentO.append([head, link])
 
                             
-        #Another way to get everything, more efficient but its not always supported
+        #Another way to get everything, more efficient but its not always supported.
         elif (self.scrapingStyle == "select"):
             for x in range(10):
-                cssSelector = (self.tag).replace("~°Ñ°~", f"{x}") #Setting up the selector 
-                selectedHtml = soup.select(cssSelector) 
-                if type(cssSelector) == str:
+                cssSelector = (self.tag).replace("~°Ñ°~", f"{x}") #Setting up the selector. 
+                selectedHtml = soup.select(cssSelector) #Selecting.
+                if type(cssSelector) == str: #If it is a string, append a list with the headline and the link.
                     for x in selectedHtml:
-                        content.append( [(x.get_text()).strip(), (self.checkIfHasLink(x.get("href")))] ) 
+                        contentO.append( [(x.get_text()).strip(), (self.checkIfHasLink(x.get("href")))] ) 
+                        #Gets the text of the element and strips it, gets the link and uses the check if has link function on it.
 
-        return(content)
+        #Resets the value of self.searchLink
+        self.searchLink = originalSearchLink
+
+        return(contentO)
 #-------------------------------------------------------------------------------------------------------
+
 
 scholar = website( "Scholar", "https://scholar.google.com/scholar?hl=es&as_sdt=0%2C5&q=~°Ñ°~&btnG=", "",
 "h3", "gs_rt", "basic")
@@ -88,28 +103,37 @@ fullListOfPages = [scholar, researchgate, pubmed, elsevier, libgen, basesearch]
 #-------------------------------------------------------------------------------------------------------
 
 def searchResults(request):
-    content = []
     if request.method == 'POST':
+        content = []
         searchTerm = request.POST['search']
+        
         for webpage in fullListOfPages:
             returns = webpage.scraping(searchTerm)
+
+            #A Dictionary is made, with name and combination as the return of the scraping function. 
+            #This dictionary is appended to a list.
             thisWebpage = {"name" : webpage.name, "combination" : returns}
             content.append( thisWebpage )
 
-        return render(request, "searchengine/searchDisplay.html/", {"content": content})
+        fullContent = {"content" : content}
+        # content -> thisWebpage x 6 (dict) -> combination (list) -> lists x (unknown) -> head and link
+        #                                   -> name 
+        return render(request, "searchengine/searchDisplay.html/", fullContent)
+
     else:
-        return render(request, "searchengine/searchDisplay.html", {"content": [] })  #content used to be as the if
+        return render(request, "searchengine/searchDisplay.html")
 
 #--------------------------------------------------------------------------------------------------------
 
+#This makes a placeholder website, just made for testing
 def placeholder(request):
     content = []
     returns = []
+
     for x in range(9):
         returns.append("lorem ipsum")
 
     for webpage in fullListOfPages:
-
         thisWebpage = {"name" : webpage.name, "combination" : returns}
         content.append( thisWebpage )
 
